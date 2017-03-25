dc9289bf1d74b6917b7ed6748e97c7667b954707
Basofe <helder_m_p_novais@hotmail.com>
2017-03-25 17:31:40 +0000

    Average orientation done

--- trafficSignsDetectionmaster/src/main/java/com/example/trafficsignsdetection/GeoLocation/FusedLocationSingleton.java
+++ trafficSignsDetectionmaster/src/main/java/com/example/trafficsignsdetection/GeoLocation/FusedLocationSingleton.java
@@ -140,6 +140,15 @@ public class FusedLocationSingleton implements GoogleApiClient.ConnectionCallbac
      */
     public Location getLastLocation() {
         if (null != mGoogleApiClient && mGoogleApiClient.isConnected()) {
+            if (ActivityCompat.checkSelfPermission(FusedLocationSingletonAPP.getAppContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(FusedLocationSingletonAPP.getAppContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+            }
             return LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         }
         else {
--- trafficSignsDetectionmaster/src/main/java/com/example/trafficsignsdetection/SensorActivityTest.java
+++ trafficSignsDetectionmaster/src/main/java/com/example/trafficsignsdetection/SensorActivityTest.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 
 import com.example.trafficsignsdetection.GeoLocation.Const;
 import com.example.trafficsignsdetection.GeoLocation.FusedLocationSingleton;
+import com.example.trafficsignsdetection.Sensors.BearingToNorthProvider;
 
 import java.util.ArrayList;
 
@@ -23,30 +24,17 @@ import java.util.ArrayList;
  * Created by helde on 21/03/2017.
  */
 
-public class SensorActivityTest extends Activity implements SensorEventListener {
+public class SensorActivityTest extends Activity implements SensorEventListener, BearingToNorthProvider.ChangeEventListener {
 
     FusedLocationSingleton fusedInstance;
+    private BearingToNorthProvider mBearingProvider;
 
     TextView Az;
-    TextView X;
-    TextView Y;
-    TextView Z;
     TextView speedarinho;
 
     String speed = "0 km/h";
 
-    double[] linear_acceleration = new double[3];
-    double[] gravity = new double[3];
-    double azimuth;  // View to draw a compass
-
-    double interval = 0.0;
-    double lastEvent = 0.0;
-    double v0 = 0.0;
-    double lastX = 0.0;
-    double lastY = 0.0;
-    double lastZ = 0.0;
-
-    ArrayList<Double> velocities = new ArrayList<>();
+    double azimuth = 0.0;
 
     private Sensor accelerometer, magnetometer;
     private SensorManager SM;
@@ -59,30 +47,15 @@ public class SensorActivityTest extends Activity implements SensorEventListener 
         fusedInstance = FusedLocationSingleton.getInstance();
         fusedInstance.startLocationUpdates();
 
-        /*X = (TextView) findViewById(R.id.textX);
-        Y = (TextView) findViewById(R.id.textY);
-        Z = (TextView) findViewById(R.id.textZ);*/
         Az = (TextView) findViewById(R.id.textAz);
         speedarinho = (TextView) findViewById(R.id.textVelocidade);
 
         SM = (SensorManager)getSystemService(SENSOR_SERVICE);
         accelerometer = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         magnetometer = SM.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-    }
 
-    private void calculateVelocity(double x, double y, double z){
-        long now = System.currentTimeMillis();
-        interval = (now - lastEvent);
-        if(interval > 100){
-            lastEvent = now;
-            double acceleration = x+y+z-lastX-lastY-lastZ;
-            double velocity = v0 + (acceleration*(interval/(double)1000));
-            //velocities.add(Math.abs(velocity));
-            v0 = velocity;
-            lastX = x;
-            lastY = y;
-            lastZ = z;
-        }
+        mBearingProvider = new BearingToNorthProvider(this);
+        mBearingProvider.setChangeEventListener(this);
     }
 
     private BroadcastReceiver mLocationUpdated = new BroadcastReceiver() {
@@ -92,11 +65,14 @@ public class SensorActivityTest extends Activity implements SensorEventListener 
             Location location = (Location) intent.getParcelableExtra(Const.LBM_EVENT_LOCATION_UPDATE);
             double velocidade = 0.0;
 
+            //Method getSpeed() gives the velocity in m/s. We need to multiple by 3600 and divide that by 1000 to convert it to km/h.
             if(location.hasSpeed())
-                velocidade = (location.getSpeed()*3600/1000);
+                velocidade = Math.round((location.getSpeed()*3600/1000) * 10.0 / 10.0);
 
             speed = velocidade + " km/h";
-            //Toast.makeText(getApplicationContext(), coordinates, Toast.LENGTH_SHORT).show();
+
+            speedarinho.setText(speed);
+
         } catch (Exception e) {
             //
         }
@@ -109,6 +85,8 @@ public class SensorActivityTest extends Activity implements SensorEventListener 
 
         SM.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
         SM.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+
+        mBearingProvider.start();
         // start location updates
         fusedInstance.startLocationUpdates();
         // register observer for location updates
@@ -120,6 +98,9 @@ public class SensorActivityTest extends Activity implements SensorEventListener 
     protected void onPause() {
         super.onPause();
         SM.unregisterListener(this);
+
+        mBearingProvider.stop();
+
         // stop location updates
         fusedInstance.stopLocationUpdates();
         // unregister observer
@@ -132,7 +113,7 @@ public class SensorActivityTest extends Activity implements SensorEventListener 
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        /*if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
             mGravity = event.values;
         if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
             mGeomagnetic = event.values;
@@ -143,17 +124,20 @@ public class SensorActivityTest extends Activity implements SensorEventListener 
             if (success) {
                 float orientation[] = new float[3];
                 SensorManager.getOrientation(R, orientation);
-                azimuth = - orientation[0] * 360 / (2 * 3.14159f);; // orientation contains: azimut, pitch and roll
+                azimuth = orientation[0] * 360 / (2 * Math.PI); // orientation contains: azimut, pitch and roll
             }
         }
 
-        Az.setText(String.valueOf(Math.round(azimuth * 100.0) / 100.0));
-
-        speedarinho.setText(speed);
+        speedarinho.setText(String.valueOf(Math.round(azimuth * 10.0) / 10.0));*/
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    @Override
+    public void onBearingChanged(double bearing) {
+        Az.setText(String.valueOf(Math.round(bearing * 10.0) / 10.0));
     }
 }
